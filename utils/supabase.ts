b69957a40277cb60647050b2f2a187f4512eccaa
Vandevelde Dimitri <dimitri.vandevelde@hotmail.com>
2026-02-09 17:24:50 +0100

    chore(config): setup supabase client and environment variables

---
+++ utils/supabase.ts
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error(
+        "Supabase URL or Key is not defined in environment variables",
+    );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
