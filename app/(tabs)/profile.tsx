fadf25ff4ac04009bb4dd39efda3ac7714990485
Vandevelde Dimitri <dimitri.vandevelde@hotmail.com>
2026-02-14 10:56:55 +0100

    feat(ui): add logout functionality to ProfileScreen with AppButton

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,17 @@
+import { supabase } from "@/src/infrastructure/supabase";
+import { AppButton } from "@/src/presentation/components/ui/AppButton";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function ProfileScreen() {
+    const logout = async () => {
+        await supabase.auth.signOut();
+    };
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Profile</Text>
             <Text style={styles.subtitle}>Welcome to your profile</Text>
+            <AppButton title="Déconnexion" variant="danger" onPress={logout} />
         </View>
     );
 }
