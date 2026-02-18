7f39b38b2f3004143f29157f7ca09b344eed5dc1
Vandevelde Dimitri <121523483+vandevelde-dimitri@users.noreply.github.com>
2026-02-18 13:21:15 +0100

    Update ChatRepository.ts

--- src/domain/repositories/ChatRepository.ts
+++ src/domain/repositories/ChatRepository.ts
@@ -1,5 +1,8 @@
 import { Conversation } from "../entities/chat/Conversation";
+import { Message } from "../entities/chat/Message";
 
 export interface ChatRepository {
     getUserConversations(userId: string): Promise<Conversation[]>;
+    getMessages(conversationId: string, page: number, pageSize: number, userId: string): Promise<Message[]>;
+    sendMessage(conversationId: string, senderId: string, content: string): Promise<void>;
 }
