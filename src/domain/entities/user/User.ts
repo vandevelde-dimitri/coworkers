86b2f32f248b4bbc795c5738a2440922fe8f4833
vandevelde dimitri <dimitri.vandevelde@hotmail.com>
2026-02-25 18:30:21 +0100

    feat(account): refactor EditProfileScreen and add useUpdateUser hook for user updates

--- src/domain/entities/user/User.ts
+++ src/domain/entities/user/User.ts
@@ -1,38 +1,41 @@
 import { UserContract, UserTeam } from "./User.enum";
 
 export interface User {
-    id: string;
-    firstName: string;
-    lastName: string;
-    profileAvatar: string;
-    city: string;
-    team: UserTeam;
-    avatarUpdatedAt?: string;
-    contract: UserContract;
-    fcName: string;
-    fcId: string;
-    memberSince: Date;
-    settings: {
-        toConvey: boolean;
-    };
+  id: string;
+  firstName: string;
+  lastName: string;
+  profileAvatar: string;
+  city: string;
+  team: UserTeam;
+  avatarUpdatedAt?: string;
+  contract: UserContract;
+  fcName: string;
+  fcId: string;
+  memberSince: Date;
+  settings: {
+    toConvey: boolean;
+  };
 }
 
 export interface UserPublic {
-    id: string;
-    firstName: string;
-    lastName: string;
-    profileAvatar: string;
-    city: string;
-    team: UserTeam;
-    avatarUpdatedAt?: string;
-    contract: UserContract;
-    fcName: string;
-    fcId: string;
-    memberSince: Date;
+  id: string;
+  firstName: string;
+  lastName: string;
+  profileAvatar: string;
+  city: string;
+  team: UserTeam;
+  avatarUpdatedAt?: string;
+  contract: UserContract;
+  fcName: string;
+  fcId: string;
+  memberSince: Date;
 }
 
 export type UpdateUserPayload = Partial<
-    Omit<User, "id" | "memberSince" | "settings">
+  Omit<
+    User,
+    "id" | "memberSince" | "settings" | "avatarUpdatedAt" | "profileAvatar"
+  >
 >;
 
 export type CreateUserPayload = Omit<User, "id" | "memberSince">;
