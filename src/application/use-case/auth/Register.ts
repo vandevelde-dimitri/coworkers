5933499aa2b61f1eeafe67daf944133188b891f1
vandevelde dimitri <dimitri.vandevelde@hotmail.com>
2026-03-12 13:21:59 +0100

    feat(auth): add email and password validation in registration process

--- src/application/use-case/auth/Register.ts
+++ src/application/use-case/auth/Register.ts
@@ -1,9 +1,28 @@
 import { IAuthRepository } from "@/src/domain/repositories/auth/AuthRepository";
 
 export class RegisterUseCase {
-    constructor(private authRepo: IAuthRepository) {}
+  constructor(private authRepo: IAuthRepository) {}
 
-    async execute(email: string, password: string): Promise<void> {
-        await this.authRepo.register(email, password);
+  async execute(email: string, password: string): Promise<void> {
+    this.validateEmail(email);
+    this.validatePassword(password);
+
+    await this.authRepo.register(email, password);
+  }
+
+  private validateEmail(email: string): void {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      throw new Error("email not valid");
     }
+  }
+
+  private validatePassword(password: string): void {
+    // Regex : 1 minuscule, 1 majuscule, 1 chiffre, 1 caractère spécial, 8+ caractères
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      throw new Error("pwd not valid");
+    }
+  }
 }
--- src/presentation/hooks/mutations/useRegister.tsx
+++ src/presentation/hooks/mutations/useRegister.tsx
@@ -2,7 +2,6 @@ import { RegisterUseCase } from "@/src/application/use-case/auth/Register";
 import { Register } from "@/src/domain/entities/auth/Register";
 import { SupabaseAuthRepository } from "@/src/infrastructure/repositories/auth/SupabaseAuthRepository";
 import { useMutation } from "@tanstack/react-query";
-import { Alert } from "react-native";
 import { CustomToast } from "../../components/ui/CustomToast";
 import { useToast } from "../../components/ui/molecules/Toast";
 
@@ -15,7 +14,15 @@ export const useRegister = () => {
     mutationFn: (payload: Register) =>
       useCase.execute(payload.email, payload.password),
     onError: (error) => {
-      toast.show(<CustomToast title="Erreur" message="Inscription échouée" />, {
+      let message = "Une erreur est survenue.";
+
+      if (error.message === "email not valid") {
+        message = "Le format de l'email est invalide.";
+      } else if (error.message === "pwd not valid") {
+        message = "Le mot de passe ne respecte pas les critères de sécurité.";
+      }
+
+      toast.show(<CustomToast title="Erreur" message={message} />, {
         type: "error",
       });
     },
